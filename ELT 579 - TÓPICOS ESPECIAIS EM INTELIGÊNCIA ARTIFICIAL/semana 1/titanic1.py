d956fb241fb9abc6c6dacf516fcc2765c0845a31
Gustavo Teixeira <contact@gustavoteixeira.dev>
2023-11-01 03:24:09 -0300

    score 0.78708

--- ELT 579 - TÓPICOS ESPECIAIS EM INTELIGÊNCIA ARTIFICIAL/semana 1/titanic1.py
+++ ELT 579 - TÓPICOS ESPECIAIS EM INTELIGÊNCIA ARTIFICIAL/semana 1/titanic1.py
@@ -38,6 +38,7 @@ X_train = train.drop(['PassengerId', 'Survived'], axis = 1)
 X_test = test.drop(['PassengerId'], axis = 1)
 
 #%% Criar feature
+import re
 
 def criar_features(X):
     subs = {'female':1, 'male':0}
@@ -57,6 +58,15 @@ def criar_features(X):
     
     X['ehIdoso'] = 0
     X['ehIdoso'] = np.where(X['Age'] > 55, 1, 0)
+
+    X['temCabine'] = np.where(pd.notnull(X['Cabin']), 1, 0)
+
+    X['contemApenasNumerosNoTicket'] = X['Ticket'].apply(lambda x: 1 if x.isdigit() else 0)
+
+    X['contemLetrasNoTicket'] = X['Ticket'].apply(lambda x: 1 if re.search('[a-zA-Z]', x) else 0)
+
+    X['tamanhoDoTicket'] = X['Ticket'].apply(lambda x: len(x))
+
     return X
 
 
@@ -64,34 +74,16 @@ X_train = criar_features(X_train)
 X_test = criar_features(X_test)
 
 
-
 #%% Selecionar as features
 
 
 features = ['Pclass', 'Age', 'SibSp',
-       'Parch', 'Fare', 'mulher', 'porto', 'ehCrianca', 'ehIdoso']
+       'Parch', 'Fare', 'mulher', 'porto', 'ehCrianca', 'ehIdoso', 'temCabine',
+       'contemApenasNumerosNoTicket', 'contemLetrasNoTicket',  'tamanhoDoTicket']
 X_train = X_train[features]
 X_test = X_test[features]
 
 y_train = train['Survived']
-
-#%% Visualização
-
-for i in X_train.columns:
-    plt.hist(X_train[i])
-    plt.title(i)
-    plt.show()
-  
-#%% Groupy
-
-gp = train.groupby(['Survived']).count()
-
-#%% pivot_table
-
-table = pd.pivot_table(train, index = ['Survived'], columns = ['Pclass'], values = 'PassengerId', aggfunc = 'count')
-
-
-
 
 
 #%% Padronizacao das variaveis
@@ -99,62 +91,6 @@ scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 
 X_test = scaler.transform(X_test)
-
-
-#%% Modelo e validacao cruzada
-
-# Logistic Regression
-from sklearn.linear_model import LogisticRegression
-# from sklearn.metrics import confusion_matrix
-# from sklearn.preprocessing import StandardScaler
-# pip install scikit-learn
-model_lr = LogisticRegression(max_iter = 1000, random_state=0)
-score = cross_val_score(model_lr, X_train, y_train, cv = 10)
-
-model_lr_score =  np.mean(score)
-print("LogisticRegression: ", model_lr_score)
-
-#%% Naive Bayes
-
-from sklearn.naive_bayes import GaussianNB
-
-model_nb = GaussianNB()
-score = cross_val_score(model_nb, X_train, y_train, cv = 10)
-
-model_nb_score = np.mean(score)
-print("GaussianNB: ", model_nb_score)
-
-#%% Nearest Neighbors Classification
-
-from sklearn.neighbors import KNeighborsClassifier
-
-model_knc = KNeighborsClassifier(n_neighbors=10)
-score = cross_val_score(model_knc, X_train, y_train, cv = 10)
-
-model_knc_score = np.mean(score)
-print("Nearest Neighbors Classification: ", model_knc_score)
-
-#%% SVC
-
-from sklearn.svm import SVC
-from sklearn.preprocessing import StandardScaler
-
-model_svc = SVC(C=1, kernel='linear', degree=2, gamma=0.01)
-score = cross_val_score(model_svc, X_train, y_train, cv = 10)
-
-model_svc_score = np.mean(score)
-print("SVC: ", model_nb_score)
-
-
-#%% Decision tree
-
-from sklearn.tree import DecisionTreeClassifier
-
-model_dt = DecisionTreeClassifier(random_state=0, max_depth=3)
-score = cross_val_score(model_dt, X_train, y_train, cv = 10)
-
-model_dt_score = np.mean(score)
-print("Decision tree: ", model_dt_score)
 
 #%% Random Forest
 from sklearn.ensemble import RandomForestClassifier
@@ -164,50 +100,7 @@ model_rf = RandomForestClassifier(n_estimators=80, max_depth=3, random_state=0)
 score = cross_val_score(model_rf, X_train, y_train, cv = 10)
 
 model_rf_score = np.mean(score)
-print("Decision tree: ", model_rf_score)
-
-#%% Otimização de hiperparametros
-from skopt import gp_minimize
-
-parametros =[('entropy', 'gini'), 
-             (100, 1000),
-             (3, 20),
-             (2,10),
-             (1, 10)]
-
-def treinar_modelo(parametros):
-    model_rf = RandomForestClassifier(
-                                      criterion=parametros[0],
-                                      n_estimators=parametros[1],
-                                      max_depth=parametros[2],
-                                      min_samples_split=parametros[3],
-                                      min_samples_leaf=parametros[4],
-                                      random_state=0)
-
-    score = cross_val_score(model_rf, X_train, y_train, cv = 10)
-
-    mean_score = np.mean(score)
-    
-    return -mean_score
-otimos = gp_minimize(treinar_modelo, parametros, random_state=0, verbose=1, n_calls=50, n_random_starts=10)
-
-
-#%% Ensamble
-
-from sklearn.ensemble import VotingClassifier
-
-model_voting = VotingClassifier(estimators==[('LR', model_lr),
-                                             ('KNN', model_knc),
-                                             ('SVC', model_svc),
-                                             ('RF', model_rf)],
-                                voting='hard')
-
-model_voting.fit(X_train, y_train)
-
- score = cross_val_score(model_voting, X_train, y_train, cv = 10)
-
- model_voting_score = np.mean(score)
- print("Decision tree: ", model_voting_score)
+print(" Random Forest  : ", model_rf_score)
 
 
 #%% Modelo final
