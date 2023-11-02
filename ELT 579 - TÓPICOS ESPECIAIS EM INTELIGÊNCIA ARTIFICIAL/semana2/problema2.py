02da1083afc62b9ae7e06ea3fecbee49c7b045c6
Gustavo Teixeira <contact@gustavoteixeira.dev>
2023-11-02 19:30:06 -0300

    codigo final semana 2

--- ELT 579 - TÓPICOS ESPECIAIS EM INTELIGÊNCIA ARTIFICIAL/semana2/problema2.py
+++ ELT 579 - TÓPICOS ESPECIAIS EM INTELIGÊNCIA ARTIFICIAL/semana2/problema2.py
@@ -23,15 +23,67 @@ scaler = StandardScaler()
 X_train_sc = scaler.fit_transform(X_train)
 X_test_sc = scaler.transform(X_test)
 
+#%% recuperar os nomes nos dataframes
+X_train_sc = pd.DataFrame(X_train_sc, columns=X_train.columns)
+X_test_sc = pd.DataFrame(X_test_sc, columns=X_train.columns)
+
+#%% Selecao de Features
+from sklearn.feature_selection import RFE
+
+max_features = 20
+score_list = list()
+for i in range(1, max_features+1):
+    modelo_linear = LinearRegression()
+
+    selector = RFE(modelo_linear, n_features_to_select=i, step=1)
+    selector = selector.fit(X_train_sc, y_train)
+
+    mask = selector.support_
+
+    features = X_train_sc.columns
+    sel_features = features[mask]
+
+    X_sel = X_train_sc[sel_features]
+
+    score_sel = cross_val_score(modelo_linear, X_sel, y_train, cv=10, scoring='r2')
+    mean_score = np.mean(score_sel)
+    print("i: " + str(i) + " = " + str(mean_score))
+    score_list.append(score_sel)
+
+#%% Plot dos scores
+import matplotlib.pyplot as plt
+
+plt.plot(score_list)
+plt.show()
+
+#%% Selecao final de features
+
+modelo_linear = LinearRegression()
+
+selector = RFE(modelo_linear, n_features_to_select=10, step=1)
+selector = selector.fit(X_train_sc, y_train)
+
+mask = selector.support_
+
+features = X_train_sc.columns
+sel_features = features[mask]
+
+X_sel = X_train_sc[sel_features]
+
+score_sel = cross_val_score(modelo_linear, X_sel, y_train, cv=10, scoring='r2')
+print(np.mean(score_sel))
+print(sel_features)
+
+
 # %% Validacao cruzada
 modelo_linear = LinearRegression()
-score = cross_val_score(modelo_linear, X_train, y_train, cv=10, scoring='r2')
+score = cross_val_score(modelo_linear, X_sel, y_train, cv=10, scoring='r2')
 
 np.mean(score)
 
 # %% Modelo final
 modelo_linear = LinearRegression()
-modelo_linear.fit(X_train_sc, y_train)
+modelo_linear.fit(X_sel, y_train)
 
 #%% Visualizacao dos coeficientes ( importancia das features)
 coef = modelo_linear.coef_
@@ -41,4 +93,20 @@ print(coef)
 features = pd.DataFrame(coef)
 features['features'] = X_train.columns
 
-# %%
+# %% testar nos dados de teste
+
+y_pred = modelo_linear.predict(X_test_sc[sel_features])
+
+r2 = modelo_linear.score(X_test_sc[sel_features], y_test)
+
+print('r2', r2)
+
+#%% outras metricas
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+rmse = mean_squared_error(y_test, y_pred, squared=False)
+
+mae = mean_absolute_error(y_test, y_pred)
+
+print('rmse', rmse)
+print('mae', mae)
+
